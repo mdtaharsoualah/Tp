b3ed634501ae4bec0e3c7ff3a45eb79f56817dea
mdtaharsoualah <42493603+mdtaharsoualah@users.noreply.github.com>
2020-11-18 21:51:25 +0100

    Update display.c

--- TpT2SOUALAH/display.c
+++ TpT2SOUALAH/display.c
@@ -7,8 +7,8 @@ void messageDisplay(volatile MSG_BLOCK* mBlock){
 	unsigned int i; 
 	messageCheck(mBlock);
 	printf("Message\n");
-	D(printf("["));
+	printf("[");
 	for(i=0;i < DATA_SIZE;i++)
-		D(printf("%u ",mBlock->mData[i]));
-	D(printf("]\n"));
+		printf("%u ",mBlock->mData[i]);
+	printf("]\n");
 }
